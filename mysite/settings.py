e6cb4c9a8285d860a3675045fb2edb6bbae6af43
sahil athrij <sahilathrij@gmail.com>
2019-08-09 06:05:04 +0000

    settings.py edited online with Bitbucket

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'testdb',
-        'USER': 'soewaise',
+        'USER': 'postgres',
         'PASSWORD': 'cusat',
         'HOST': '127.0.0.1',
         'PORT': '5432',
