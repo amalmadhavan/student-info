e22eb729fdd0320592d48cd1c1d9b410e7833757
Jyothis P <jyothisp52@gmail.com>
2019-12-30 04:23:23 +0530

    debug2

--- student/views.py
+++ student/views.py
@@ -801,7 +801,7 @@ def loguser(request):
             passd = request.POST['password']
             logger.info("URL %s calls loguser ", request.get_full_path())
             print('00=============')
-            print(CustomUser.objects())
+            print(CustomUser.objects)
             valid_user = CustomUser.objects.filter(username=uname)
             print(valid_user)
             print('11=============')
